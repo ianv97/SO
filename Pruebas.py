df648d249194bfb152cbfc5dfc7c5dd2e54c0f89
Ian Vaernet <ianalexisnet@hotmail.com>
2018-08-31 14:36:08 -0300

    Ventana de prueba

--- Pruebas.py
+++ Pruebas.py
@@ -1,1 +1,22 @@
-print("Prueba")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Ventana(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.iniciarGUI()
+
+    def iniciarGUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Simulador de Sistema Operativo')
+        ##self.setWindowIcon(QIcon('web.png'))
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ventana = Ventana()
+    sys.exit(app.exec_())
